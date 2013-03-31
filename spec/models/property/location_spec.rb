103763843a32fe4b29ca252008afe8f7c3ceda82
Kristian Mandrup <kmandrup@gmail.com>
2013-03-31 21:59:33 +0200

    updated deps

--- spec/models/property/location_spec.rb
+++ spec/models/property/location_spec.rb
@@ -3,26 +3,24 @@ require 'spec_helper'
 describe Property::Location do
   subject { property }
 
-  context 'Property' do
-    let(:property)  { create :valid_property, street: street, city: city }
-    let(:street)    { 'Griffenfeldsgade 11b' }
-    let(:country)   { 'Denmark' }
-    let(:city)      { 'Copenhagen' }
+  let(:property)  { create :valid_property, street: street, city: city }
+  let(:street)    { 'Griffenfeldsgade 11b' }
+  let(:country)   { 'Denmark' }
+  let(:city)      { 'Copenhagen' }
 
-    # http://www.infoplease.com/ipa/A0001769.html
-    # Latitude and Longitude of World Cities
-    # Copenhagen, Denmark, 55, 40 N, 12, 34 E
-    let(:latitude)  { 55.688568 }
-    let(:longitude) { 12.5549331 }
+  # http://www.infoplease.com/ipa/A0001769.html
+  # Latitude and Longitude of World Cities
+  # Copenhagen, Denmark, 55, 40 N, 12, 34 E
+  let(:latitude)  { 55.688568 }
+  let(:longitude) { 12.5549331 }
 
-    describe 'location' do
-      it 'should set latitude' do
-        expect(subject.latitude).to be_within(0.5).of latitude
-      end
+  describe 'location' do
+    it 'should set latitude' do
+      expect(subject.latitude).to be_within(0.5).of latitude
+    end
 
-      it 'should set longitude' do
-        expect(subject.longitude).to be_within(0.5).of longitude
-      end
+    it 'should set longitude' do
+      expect(subject.longitude).to be_within(0.5).of longitude
     end
   end
 end
